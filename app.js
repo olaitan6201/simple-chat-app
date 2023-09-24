f2de89c3c67355a9752f57570807433b7a9142a0
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-09-24 11:50:29 +0100

    removed node_modules folder

--- app.js
+++ app.js
@@ -27,4 +27,8 @@ function onConnected(socket){
     socket.on('message', (data) => {
         socket.broadcast.emit('chat-message', data)
     })
+
+    socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
+    })
 }
--- public/main.js
+++ public/main.js
@@ -1,32 +1,75 @@
 const socket = io()
 
+// UI element refs
 const clientsTotal = document.getElementById('clients-total')
 const messageContainer = document.getElementById('message-container')
 const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput = document.getElementById('message-input')
 
+const messageTone = new Audio('./message-tone.mp3')
+
+// Handle form submit
 messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     sendMessage()
 })
 
+// Handle message input focus
+messageInput.addEventListener('focus', (e) => {
+    socket.emit('feedback', {
+        feedback: `✍️ ${nameInput.value} is typing . . .`
+    })
+})
+
+// Handle message input keypress
+messageInput.addEventListener('keypress', (e) => {
+    socket.emit('feedback', {
+        feedback: `✍️ ${nameInput.value} is typing . . .`
+    })
+})
+
+// Handle message input blur event
+messageInput.addEventListener('blur', (e) => {
+    socket.emit('feedback', {
+        feedback: ''
+    })
+})
+
+// Listening to clients-total event
 socket.on('clients-total', (data) => {
     clientsTotal.innerText = `Total Clients: ${data}`
 })
 
+// Listening to chat-message broadcast event
 socket.on('chat-message', (data) => {
     addMessageToUI(false, data)
+    messageTone.play()
 })
 
+// Listening to feedback broadcast event
+socket.on('feedback', ({feedback}) => {
+    clearFeedback()
+
+    const elem = `<li class="message-feedback">
+        <p class="feedback" id="feedback">
+            ${feedback}
+        </p>
+    </li>`
+
+    messageContainer.innerHTML += elem
+})
+
+
+// send message function
 function sendMessage() {
     const name = nameInput.value
     const message = messageInput.value
     if(name.trim().length === 0 || message.trim().length === 0){
         return;
     }
-    
+
     const data = {
         name,
         message,
@@ -38,7 +81,10 @@ function sendMessage() {
     messageInput.value = ''
 }
 
+// add message to ui function
 function addMessageToUI(isOwnMessage, {message, name, dateTime}) {
+    clearFeedback()
+
     const elem = `<li class="${isOwnMessage ? 'message-right' : 'message-left'}">
         <p class="message">
             ${message}
@@ -51,6 +97,13 @@ function addMessageToUI(isOwnMessage, {message, name, dateTime}) {
     scrollToBottom()
 }
 
+// scroll to bottom function
 function scrollToBottom() {
     messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
+
+function clearFeedback(){
+    document.querySelectorAll('li.message-feedback').forEach(elem => {
+        elem.parentNode.removeChild(elem)
+    })
 }
