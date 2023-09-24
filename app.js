96d8bd8d56516ab5cb9a63680aae7c1665223b43
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-09-24 10:30:03 +0100

    connected socket.io -- handles connected and disconnected events -- emits clients-total property

--- app.js
+++ app.js
@@ -4,5 +4,23 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => console.log(`Chat server on port ${PORT}`))
 
+const io = require('socket.io')(server)
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+let socketsConnected = new Set()
+
+io.on('connection', onConnected)
+
+function onConnected(socket){
+    console.log(`Socket connected: ${socket.id}`);
+    socketsConnected.add(socket.id)
+
+    io.emit('clients-total', socketsConnected.size)
+
+    socket.on('disconnect', () => {
+        console.log(`Socket disconnected: ${socket.id}`);
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+    })
+}
