f09978443176833d66981bfa9d64683a00cab40b
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-09-24 11:06:16 +0100

    handled: -- listening to 'message' -- broadcasting 'chat-message' events -- sending and listening to broadcasted messages

--- app.js
+++ app.js
@@ -23,4 +23,8 @@ function onConnected(socket){
         socketsConnected.delete(socket.id)
         io.emit('clients-total', socketsConnected.size)
     })
+
+    socket.on('message', (data) => {
+        socket.broadcast.emit('chat-message', data)
+    })
 }
--- public/main.js
+++ public/main.js
@@ -1,7 +1,56 @@
 const socket = io()
 
 const clientsTotal = document.getElementById('clients-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    sendMessage()
+})
 
 socket.on('clients-total', (data) => {
     clientsTotal.innerText = `Total Clients: ${data}`
 })
+
+socket.on('chat-message', (data) => {
+    addMessageToUI(false, data)
+})
+
+function sendMessage() {
+    const name = nameInput.value
+    const message = messageInput.value
+    if(name.trim().length === 0 || message.trim().length === 0){
+        return;
+    }
+    
+    const data = {
+        name,
+        message,
+        dateTime: new Date()
+    }
+
+    socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
+}
+
+function addMessageToUI(isOwnMessage, {message, name, dateTime}) {
+    const elem = `<li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+        <p class="message">
+            ${message}
+            <span>${name} ⚪ ${moment(dateTime).fromNow()}</span>
+        </p>
+    </li>`
+
+    messageContainer.innerHTML += elem
+
+    scrollToBottom()
+}
+
+function scrollToBottom() {
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
